6a1952ab5a621de83198ccc84c3ea6f982af6a99
Thanh-Dat Nguyen <112456112+Dat24DAT@users.noreply.github.com>
2024-12-23 12:12:39 +0700

    Delete encryption_128 directory

--- encryption_128/rtl/rotationword.sv
+++
@@ -1,7 +0,0 @@
-module rotationword (
-   input logic [31:0] word_in,
-   output logic [31:0] word_out
-);
-  assign word_out = {word_in[23:0],
-                     word_in[31:24]};
-endmodule
--- encryption_128/sim/script/add_wave.do
+++
@@ -1,2 +0,0 @@
-add wave -group TB *
-add wave -group DUT /encryption_128_inst/*
